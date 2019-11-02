5dbd240ed71e73b8b04d72d80cf55128f6367706
youssef-emam <46711195+youssef-emam@users.noreply.github.com>
2019-11-02 08:57:13 +0000

    Update cleandb48.sql

--- Scripts/cleandb48.sql
+++ Scripts/cleandb48.sql
@@ -2,3 +2,13 @@
 
 DROP SCHEMA IF EXISTS `qubaccomodation48`;
 CREATE SCHEMA IF NOT EXISTS `qubaccomodation48`;
+DROP TABLE Buildings;
+DROP TABLE Apartment;
+DROP TABLE Person;
+DROP TABLE Tenant;
+DROP TABLE Employee;
+DROP TABLE Manager;
+DROP TABLE TechnicianSkill;
+DROP TABLE Skill;
+DROP TABLE Lease;
+DROP TABLE LeaseTenant;
