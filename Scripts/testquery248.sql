0c4a030a0039c197ea9b6179ec6bb42aa54ae698
The-Boat <radair2842@gmail.com>
2019-11-04 11:55:13 +0000

    Query Test Scripts

---
+++ Scripts/testquery248.sql
@@ -0,0 +1,3 @@
+-- INSERT INTO employee (PersonID, Pay) VALUES ('1','15000.00'),('2','15000.00'),('3','15000.00');
+-- apartmentmanagerINSERT INTO Manager (EmployeeID) VALUES ('1'),('2'),('3');
+-- INSERT INTO apartmentmanager(managerid,apartmentid)
