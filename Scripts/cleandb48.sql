ecb77f0b3e80fcb89b1d6839c399e2f75d1ed516
The-Boat <radair2842@gmail.com>
2019-11-04 11:21:33 +0000

    Altered clean

--- Scripts/cleandb48.sql
+++ Scripts/cleandb48.sql
@@ -1,14 +1,17 @@
 -- Clean first draft, just drops schema.
 -- second draft
-DROP SCHEMA IF EXISTS `qubaccomodation48`;
-CREATE SCHEMA IF NOT EXISTS `qubaccomodation48`;
-DROP TABLE IF EXISTS Buildings;
+DROP TABLE IF EXISTS leasemanager;
+DROP TABLE IF EXISTS apartmentmanager;
+DROP TABLE IF EXISTS ManagerOffice;
+DROP TABLE IF EXISTS TechnicianSkill;
+DROP TABLE IF EXISTS ApartmentBuilding;
+DROP TABLE IF EXISTS building;
 DROP TABLE IF EXISTS Apartment;
 DROP TABLE IF EXISTS Person;
 DROP TABLE IF EXISTS Tenant;
 DROP TABLE IF EXISTS Employee;
 DROP TABLE IF EXISTS Manager;
-DROP TABLE IF EXISTS TechnicianSkill;
+DROP TABLE IF EXISTS Office;
 DROP TABLE IF EXISTS Skill;
 DROP TABLE IF EXISTS Lease;
 DROP TABLE IF EXISTS LeaseTenant;
